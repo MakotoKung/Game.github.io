b8f3d177b549c1f38a7ef20dbc384b099ab23bdc
Supppasin <84625475+karnza1100@users.noreply.github.com>
2022-10-30 16:27:05 +0700

    FINALLY

--- java.js
+++ java.js
@@ -1,8 +1,14 @@
 var btn = document.getElementById('active');
 var rec = document.getElementById('rec');
 var op0 = document.getElementById('op0');
+var btn1 = document.getElementById('active1');
+var gameimg = document.getElementById('gameimg');
+var op1 = document.getElementById('op1');
 
 btn.onclick = function(){
     rec.classList.toggle('rec');
     op0.classList.toggle('op0');
+    gameimg.classList.toggle('gameimg');
+    op1.classList.toggle('op1');
 };
+
